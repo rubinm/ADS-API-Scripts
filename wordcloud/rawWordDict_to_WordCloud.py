f84837353b01c047dfae7902d387fe113b587160
Katie Frey <katieefrey@gmail.com>
2014-10-08 15:39:40 -0400

    adding readme for wordcloud script

--- wordcloud/rawWordDict_to_WordCloud.py
+++ wordcloud/rawWordDict_to_WordCloud.py
@@ -19,26 +19,27 @@ for k in shorter:
         cloudseeder.write(k+'\n')
 cloudseeder.close()
 
-#This section is very similar to above, but it exaggerates the weighting of terms.
+#This section is very similar to above, but it exaggerates the weighting of terms
+#by multiplying the number of occurances by an arbitrary amount.
 nums = list(set(num_list))
 
 def roundup(x):
     return int(math.ceil(x / 10.0)) * 10
 
-rounded = roundup(nums[-1])
-range_num = rounded / 3
+rounded = roundup(nums[-1]) #rounds up from the last number
+range_num = rounded / 3 #arbitrarily dividing into three equal groups of size range_num
 
 cloudseederex = open('cloudseeder-exaggerated'+timestamp+'.txt', 'w')
 
 for k in shorter:
     number = shorter[k]['total_occurences']
-    if number < range_num:
+    if number < range_num: #first third, multiply number by 1
         for i in range (0,number):
             cloudseederex.write(k+'\n')
-    elif range_num <= number < (range_num*2):
+    elif range_num <= number < (range_num*2): #second third, multiply number by 2
         for i in range (0,number*2):
             cloudseederex.write(k+'\n')
-    elif number >= (range_num*2):
+    elif number >= (range_num*2): #third third, mulitply number by 3
         for i in range (0,number*3):
             cloudseederex.write(k+'\n')
 cloudseederex.close()
